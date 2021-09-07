1733a527f7af0e27debbd8f39fc209a664ee2798
Cole Bish <colester32@yahoo.com>
2021-09-07 14:31:42 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Person from './Components/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={"Doe, Jane"} age={"45"} haircolor={"Black"} />
+      <Person name={"Smith, John"} age={"88"} haircolor={"Brown"} />
+      <Person name={"Fillmore, Millard"} age={"50"} haircolor={"Grey"} />
+      <Person name={"Smith, Maria"} age={"62"} haircolor={"Blonde"} />
     </div>
   );
 }
